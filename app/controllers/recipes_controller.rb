7198b380b5a864e8298d2b7db2c1632c483ae0d9
Ryan Ragasa <rjragasa@yahoo.com>
2015-08-07 07:08:48 +0000

    modify show recipes to fix bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,6 @@ class RecipesController < ApplicationController
   before_action :require_same_user, only: [:edit, :update]
   before_action :admin_user, only: :destory
   
-  
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
